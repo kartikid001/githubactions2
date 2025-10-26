26780a41ba5fc42e73a15e0102b21637e38e2211
kartikid001 <kartikideshmukh001@gmail.com>
2025-10-26 15:07:08 +0530

    API Pipeline 1

--- read_data_api.py
+++ read_data_api.py
@@ -1,7 +1,7 @@
 import requests
 import pandas as pd
 
-response = request.get("https://jsonplaceholder.typicode.com/users")
+response = requests.get("https://jsonplaceholder.typicode.com/users")
 # by defaily we get response in text format
 data = response.json()
 df=pd.DataFrame(data)
