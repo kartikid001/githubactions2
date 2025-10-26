d7a319ae86f87a3708c0bedad1552f7834f26d7b
kartikid001 <kartikideshmukh001@gmail.com>
2025-10-26 15:04:05 +0530

    API Pipeline 1

--- read_data_api.py
+++ read_data_api.py
@@ -5,4 +5,5 @@ response = request.get("https://jsonplaceholder.typicode.com/users")
 # by defaily we get response in text format
 data = response.json()
 df=pd.DataFrame(data)
+df = df[["id", "name"]]
 print(df)
