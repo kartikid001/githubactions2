fbeb94fdf5c42223ebbb754094865cca593ab9d0
kartikid001 <kartikideshmukh001@gmail.com>
2025-10-26 15:19:13 +0530

    API Pipeline 1

--- read_data_api.py
+++ read_data_api.py
@@ -1,9 +1,13 @@
 import requests
 import pandas as pd
+import os
 
-response = requests.get("https://jsonplaceholder.typicode.com/users")
-# by defaily we get response in text format
-data = response.json()
-df=pd.DataFrame(data)
-df = df[["id", "name"]]
-print(df)
+token = os.getenv("API_TOKEN")
+print(f"Token: {token}")
+
+# response = requests.get("https://jsonplaceholder.typicode.com/users")
+# # by defaily we get response in text format
+# data = response.json()
+# df=pd.DataFrame(data)
+# df = df[["id", "name"]]
+# print(df)
